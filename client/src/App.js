2a4127fcfb67800b0e97fa9566e6034a07413cd5
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-09-13 20:13:40 -0400

    added some more files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from './components/PrivateRoute';
+import BubblesPage from './components/Bubbles';
+
+
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <Switch>
+          <PrivateRoute exact path="/protected" component={BubblesPage} />
+          {/* <Route path="/login" component={Login} />
+          <Route component={Login} /> */}
+        </Switch>
       </div>
     </Router>
   );
